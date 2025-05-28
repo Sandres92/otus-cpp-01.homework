6404c46f11aefba3324b30c65d783331186b5231
Sandres92 <eastoutcity@mail.ru>
2025-05-28 20:06:49 +0000

    Пробный коммит домашнего задания

--- test_version.cpp
+++ test_version.cpp
@@ -6,8 +6,14 @@
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
-BOOST_AUTO_TEST_CASE(test_valid_version) {
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
 	BOOST_CHECK(version() > 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_valid_sum)
+{
+	BOOST_CHECK(1 + 1 == 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
