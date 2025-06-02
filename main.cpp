da3b1533b7a712dd3f165399de722e33445b03ad
Sandres92 <eastoutcity@mail.ru>
2025-06-02 19:30:25 +0000

    Test build

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int, char **)
 {
   std::cout << "Version: " << version() << std::endl;
   std::cout << "Hello, world!" << std::endl;
+  std::cout << "Hello, world22" << std::endl;
 
   return 0;
 }
